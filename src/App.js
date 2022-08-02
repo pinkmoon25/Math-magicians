7694d0d6f2b567782d43ef3e5ff5bba61eb7adb4
Tushar <singhtushar348@gmail.com>
2022-08-02 20:56:03 +0100

    Modify App to a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import Calculator from './components/calculator';
-
-function App() {
-  return (
-    <Calculator />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
-
-// const root = ReactDOM.createRoot(document.querySelector('body'));
-// root.render(<Calculator />);
