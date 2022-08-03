29a611fa431f9c4d9259846cd13ec68728c34a8e
Tushar <singhtushar348@gmail.com>
2022-08-03 21:06:29 +0100

    Add event handler and Math logic

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,4 +1,4 @@
-import Big from 'big.js';
+import Big from 'big.js'; // eslint-disable-line
 
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne);
@@ -20,7 +20,7 @@ export default function operate(numberOne, numberTwo, operation) {
     }
   }
   if (operation === '%') {
-     try {
+    try {
       return one.mod(two).toString();
     } catch (err) {
       return "Can't find modulo as can't divide by 0.";
