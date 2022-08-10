61bf6b96c62d48a5273e576bb731dd34a0b3b8c0
Tushar <singhtushar348@gmail.com>
2022-08-10 18:21:28 +0100

    Add HashRouter to fix gh-pages 404

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Header from './components/header';
 import Quote from './components/quote';
 import Home from './components/home';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
       <Route path="/quote" element={<Quote />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
